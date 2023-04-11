22e435a3860d490a8e3045b8cbac8fea387fe212
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-04-11 20:28:18 -0300

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,25 @@
-const todosOsBtns = document.querySelectorAll('.btn');
-const telaResultado = document.querySelector('.telaResultado p')
+const todosOsBtns = document.querySelectorAll(".btn");
+const telaResultado = document.querySelector(".telaResultado p");
+let botaoClicado;
 
 function adicionarCaracters(event) {
-    const botaoClicado = event.target.innerText
+    botaoClicado = event.target.innerText;
 
-    if(botaoClicado == "=" || botaoClicado == "AC" || botaoClicado == "backspace") return
+    if (
+        botaoClicado == "=" ||
+        botaoClicado == "AC" ||
+        botaoClicado == "backspace"
+    )
+        return;
 
-    telaResultado.innerHTML += botaoClicado
-
-    return botaoClicado
+    telaResultado.innerHTML += botaoClicado;
 }
 
-
+function apagarCaracter() {
+    const bntApagarTudo = document.querySelector(".btnApagarTudo");
+    const btnApagarCaracter = document.querySelector(".apagarCaracter");
+}
 
 todosOsBtns.forEach((btns) => {
-    btns.addEventListener('click', adicionarCaracters)
-})
+    btns.addEventListener("click", adicionarCaracters);
+});
