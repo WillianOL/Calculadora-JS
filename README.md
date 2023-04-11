8cb92fc799658fe7685720ba53fc4395915310cb
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-04-11 20:16:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# calculadoraJS
+ Calculadora feita com JavaScript
---
+++ assets/index.js
@@ -0,0 +1,15 @@
+const todosOsBtns = document.querySelectorAll('.btn');
+const telaResultado = document.querySelector('.telaResultado p')
+
+function adicionarCaracters(event) {
+    const botaoClicado = event.target.innerText
+
+    if(botaoClicado == "=" || botaoClicado == "AC" || botaoClicado == "backspace") return
+
+    telaResultado.innerHTML += botaoClicado 
+}
+
+
+todosOsBtns.forEach((btns) => {
+    btns.addEventListener('click', adicionarCaracters)
+})
