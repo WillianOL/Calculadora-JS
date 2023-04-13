de716a40b9be54e8e6a773f97c53db6708698001
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-04-13 09:21:16 -0300

    Ataulizações

--- assets/index.js
+++ assets/index.js
@@ -1,36 +1,41 @@
 const todosOsBtns = document.querySelectorAll(".btn");
-const btnsApagar = document.querySelectorAll('.btnApagar')
+const btnsApagar = document.querySelectorAll(".btnApagar");
 const telaResultado = document.querySelector(".telaResultado p");
+const caracterOperacao = document.querySelectorAll(".caracterEspecial");
 
 function adicionarCaracters(event) {
     const botaoClicado = event.target.innerText;
 
-    if (botaoClicado == "AC" || botaoClicado == "backspace"){
+    if (
+        botaoClicado == "=" ||
+        botaoClicado == "AC" ||
+        botaoClicado == "backspace"
+    ) {
         apagarCaracters(botaoClicado);
-        return
-    } else if(botaoClicado == "="){
-        operacao()
-        return
+        return;
     }
 
     telaResultado.innerHTML += botaoClicado;
 }
 
-function apagarCaracters(btn){
-    const totalCaracters = telaResultado.innerHTML
-    if(btn == "AC"){
-        telaResultado.innerHTML = ""
-    } else if(btn == "backspace"){
-        const removeUltimoCaracter = totalCaracters.substring(0, totalCaracters.length - 1)
-        telaResultado.innerHTML = removeUltimoCaracter
-    };
+function apagarCaracters(btn) {
+    const totalCaracters = telaResultado.innerHTML;
+    if (btn == "AC") {
+        telaResultado.innerHTML = "";
+    } else if (btn == "backspace") {
+        const removeUltimoCaracter = totalCaracters.substring(
+            0,
+            totalCaracters.length - 1
+        );
+        telaResultado.innerHTML = removeUltimoCaracter;
+    }
 }
 
-function operacao() {
-    const caracters = telaResultado.innerHTML
-    
-}
+function operacao(btn) {}
 
+caracterOperacao.forEach((btns) => {
+    btns.addEventListener("click", operacao);
+});
 
 todosOsBtns.forEach((btns) => {
     btns.addEventListener("click", adicionarCaracters);
