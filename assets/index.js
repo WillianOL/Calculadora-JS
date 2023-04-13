39c98cdeaa0356fedb21904a8f0b8436b6afa695
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-04-13 20:34:45 -0300

    Atualizações

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,6 @@
 const todosOsBtns = document.querySelectorAll(".btn");
 const btnsApagar = document.querySelectorAll(".btnApagar");
 const telaResultado = document.querySelector(".telaResultado p");
-const caracterOperacao = document.querySelectorAll(".caracterEspecial");
 
 function adicionarCaracters(event) {
     const botaoClicado = event.target.innerText;
@@ -27,14 +26,6 @@ function apagarCaracters(btn) {
         telaResultado.innerHTML = removeUltimoCaracter;
     }
 }
-
-function operacao(btn) {
-    
-}
-
-caracterOperacao.forEach((btns) => {
-    btns.addEventListener("click", operacao);
-});
 
 todosOsBtns.forEach((btns) => {
     btns.addEventListener("click", adicionarCaracters);
